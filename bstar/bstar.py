d9eb3ccbd646c997a71b44f6850237fd0307f9f8
hi <damagetesting@proton.me>
2023-11-05 00:09:36 -0400

    added json/yaml support

--- bstar/bstar.py
+++ bstar/bstar.py
@@ -4,11 +4,34 @@ import json
 import yaml
 
 class Bstar:
+    # Takes in a filepath or raw bytes as input
+    # If passing in raw bytes, please also provide a filename
     def __init__(self, data, filename=''):
         if type(data) != bytes:
-            self.filename = os.path.basename(data)
-            with open(data, 'rb') as file:
-                data = file.read()
+            self.filename = os.path.basename(data).replace('.json', '').replace('.yml', '').replace('.yaml', '').replace('.txt', '')
+            if os.path.splitext(data)[1] == '.bstar':
+                with open(data, 'rb') as file:
+                    data = file.read()
+            elif os.path.splitext(data)[1] == '.json':
+                with open(data, 'r') as file:
+                    self.entries = json.load(file)["Entries"]
+                    self.magic = "STAR"
+                    self.version = 1
+                    return
+            elif os.path.splitext(data)[1] in ['.yaml', '.yml']:
+                with open(data, 'r') as file:
+                    self.entries = yaml.safe_load(file)["Entries"]
+                    self.magic = "STAR"
+                    self.version = 1
+                    return
+            else:
+                with open(data, 'r') as file:
+                    self.entries = []
+                    for line in file:
+                        self.entries.append(line.replace('\n', ''))
+                    self.magic = "STAR"
+                    self.version = 1
+                    return
         else:
             if os.path.splitext(filename)[1] != '.bstar':
                 filename += '.bstar'
@@ -24,7 +47,9 @@ class Bstar:
         self.entries = []
         for i in range (self.count):
             self.entries.append(self.stream.read_string())
+        return
     
+    # Converts Bstar object into .bstar file
     def Serialize(self, output_dir=''):
         with open(os.path.join(output_dir, self.filename), 'wb') as outfile:
             buffer = WriteStream(outfile)
@@ -35,19 +60,23 @@ class Bstar:
             for entry in self.entries:
                 buffer.write(string(entry) + b'\x00')
 
+    # Converts Bstar object into .json file
     def ToJson(self, output_dir=''):
         with open(os.path.join(output_dir, self.filename) + ".json", 'w', encoding='utf-8') as outfile:
             json.dump({"Entries" : self.entries}, outfile, ensure_ascii=False, indent=4)
     
+    # Converts Bstar object into .yml file
     def ToYaml(self, output_dir=''):
         with open(os.path.join(output_dir, self.filename) + ".yml", 'w', encoding='utf-8') as outfile:
             yaml.dump({"Entries" : self.entries}, outfile, sort_keys=False, allow_unicode=True, encoding='utf-8')
 
+    # Converts Bstar object into .txt file
     def ToText(self, output_dir=''):
         with open(os.path.join(output_dir, self.filename) + ".txt", 'w', encoding='utf-8') as outfile:
             for entry in self.entries:
                 print(entry, file=outfile)
 
+    # Takes in a string(s) to replace and a replacement string(s) and replaces them
     def ReplaceString(self, original, replacement):
         if type(original) == list and type(replacement) == list:
             if len(original) != len(replacement):
@@ -59,6 +88,7 @@ class Bstar:
                 new_string = str(new_string)
             self.entries[self.entries.index(original)] = replacement
 
+    # Appends the provided string(s) to the end of the file
     def AddString(self, new_string):
         if type(new_string) == list:
             self.entries += new_string
@@ -67,6 +97,7 @@ class Bstar:
                 new_string = str(new_string)
             self.entries.append(new_string)
 
+    # Inserts the provided string(s) at the provided position
     def InsertString(self, position, new_string):
         if type(new_string) == list:
             self.entries[position:position] = new_string
@@ -75,6 +106,7 @@ class Bstar:
                 new_string = str(new_string)
             self.entries.insert(position, new_string)
 
+    # Removes the specified string(s) from the file
     def RemoveString(self, old_string):
         if type(old_string) == list:
             for string in old_string:
